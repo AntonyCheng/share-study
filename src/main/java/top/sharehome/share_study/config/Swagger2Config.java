204ae676113c0ce59fcbfa59634af65e253b799a
AntonyCheng <1911261716@qq.com>
2023-03-02 17:35:48 +0800

    v1.0.6 代码格式优化，教师管理的设计思想的改变，即后续重新设计一个UserController，分离平台前后台，UserController尽可能使用TeacherService里的代码

--- src/main/java/top/sharehome/share_study/config/Swagger2Config.java
+++ src/main/java/top/sharehome/share_study/config/Swagger2Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -19,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class Swagger2Config extends WebMvcConfigurationSupport {
+public class Swagger2Config implements WebMvcConfigurer {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
--- src/main/java/top/sharehome/share_study/config/WebMvcConfig.java
+++ src/main/java/top/sharehome/share_study/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.sharehome.share_study.common.jackson_mapper.JacksonObjectMapper;
 
 import java.util.List;
@@ -14,14 +15,14 @@ import java.util.List;
  * @author AntonyCheng
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
     /**
      * 添加消息转换器，
      *
      * @param converters 转换器
      */
     @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 创建消息转换器对象
         MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
         // 设置对象转换器，底层使用Jackson将Java对象转为Json
